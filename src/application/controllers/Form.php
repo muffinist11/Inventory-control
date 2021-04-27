0529896da00404eb4a5c1b14e0476b919a7157c5
muffinist_11 <pfoulcikcien@icloud.com>
2021-04-27 16:35:40 +0900

    ログイン画面表示

--- src/application/controllers/Form.php
+++ src/application/controllers/Form.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Form extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
@@ -18,8 +18,24 @@ class Welcome extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/userguide3/general/urls.html
 	 */
+
+	public function __construct()
+    {
+        // CI_Model constructor の呼び出し
+        parent::__construct();
+        $this->load->library('session');
+        $this->load->model('Form_model');
+        date_default_timezone_set('Asia/Tokyo');
+        
+    }
+
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->load->view('login');
+	}
+
+	public function adminlogin()
+	{
+		$this->load->view('adminlogin');
 	}
 }
